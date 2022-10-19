f187182bc5f4d9a1251c9f964ea637583c3eba9e
Manar <manar.qasem732016@gmail.com>
2022-10-19 22:48:07 +0300

    fix eslint

--- src/homepage.js
+++ src/homepage.js
@@ -1,9 +1,9 @@
 const gameOfThronesAPI = async () => {
-  const gettingList = await fetch("https://thronesapi.com/api/v2/Characters");
+  const gettingList = await fetch('https://thronesapi.com/api/v2/Characters');
   const convertList = await gettingList.json();
   return convertList;
 };
-const characterElement = document.getElementById("character-list");
+const characterElement = document.getElementById('character-list');
 const globalList = gameOfThronesAPI();
 const displayCharacters = async (start, range) => {
   const characterList = await globalList;
